dea650d808d3c6cd14cf354a43dbc4af2d38df1a
Quentin Dejean <quentin@cammy.com>
2016-03-19 14:57:07 +1100

    Added babelrc and 2015 dependencies Added eslint, extending Airbnb's App is now up and runnin

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,18 +1,8 @@
 import mongoose from 'mongoose';
 
-const listen = function listen () {
-  if (app.get('env') === 'test') return;
-  app.listen(port);
-  console.log('Express app started on port ' + port);
-}
-
 const connect = function connect () {
-  var options = { server: { socketOptions: { keepAlive: 1 } } };
-  return mongoose.connect(config.db, options).connection;
+  const options = { server: { socketOptions: { keepAlive: 1 } } };
+  return mongoose.connect('mongodb://localhost/restaurant');
 }
 
-
-connect()
-  .on('error', console.log)
-  .on('disconnected', connect)
-  .once('open', listen);
+export default connect;
